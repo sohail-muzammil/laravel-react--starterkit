66097c50ce6cb97a243fe4e98a9c954a656f2ab0
sohail-muzammil <sohail.muzammil20@gmail.com>
2025-04-09 15:09:12 +0800

    Admin Auth Setting

--- database/migrations/2025_04_06_040616_create_admins_table.php
+++ database/migrations/2025_04_06_040616_create_admins_table.php
@@ -23,6 +23,21 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        Schema::create('admin_password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('admin_sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('admin_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('admin_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
     /**
