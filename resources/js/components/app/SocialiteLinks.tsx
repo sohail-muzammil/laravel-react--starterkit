0f08879cef07e211648b7d6f9d6b72f256a56ff9
sohail-muzammil <sohail.muzammil20@gmail.com>
2025-04-11 19:17:28 +0800

    Minor Bug Fix

--- resources/js/components/app/SocialiteLinks.tsx
+++ resources/js/components/app/SocialiteLinks.tsx
@@ -20,7 +20,7 @@ const SocialiteLinks: React.FC = () => {
                         className="inline-flex h-9 items-center justify-center gap-2 whitespace-nowrap rounded-md border border-input bg-background px-4 py-2 text-sm font-medium capitalize shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
                     >
                         <span dangerouslySetInnerHTML={{ __html: provider.icon }}></span>
-                        <span>{provider.name} 1</span>
+                        <span>{provider.name}</span>
                     </a>
                 ))}
             </div>
