ada7788954babd2b83bfd47de7f1a8f13351893b
sohail-muzammil <sohail.muzammil20@gmail.com>
2025-04-12 15:11:33 +0800

    Redirect Home Page to Dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    // return Inertia::render('welcome');
+    return to_route('dashboard');
 })->name('home');
 
 
